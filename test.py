c89c308fcaeeae281e80f805f5dd49cac14b2338
TruongGiangPham-1 <truonggi@ualberta.ca>
2024-10-17 15:29:55 -0600

    docs: init exploring how gym env works

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import place_env
 import gym
 
-# env = gym.make("Place-v0", log_dir=None, simulator=False)
-# obs, info = env.reset()
-# print(info)
+env = gym.make("Place-v0", log_dir=None, simulator=False)
+obs, info = env.reset()
+print(info)
