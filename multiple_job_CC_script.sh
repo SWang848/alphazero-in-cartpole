5133939a786a66705edde3e20bc2f3578d147e91
SWang848 <swang848@outlook.com>
2024-12-31 22:51:45 -0700

    c30b classic MDP script

--- multiple_job_CC_script.sh
+++ multiple_job_CC_script.sh
@@ -1,6 +1,6 @@
 #!bin/bash
 for c_init in {2,2.5,3,3.5}; do
-    for num_simulations in {100,120,150}; do
+    for num_simulations in {120,150,180}; do
         sbatch CC_script.sh $c_init $num_simulations
     done
 done
