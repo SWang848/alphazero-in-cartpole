80df42a738fa9a93185a8208f8befa90c8cf55cf
SWang848 <swang848@outlook.com>
2025-01-06 23:55:27 -0700

    fix script typos

--- multiple_job_CC_script.sh
+++ multiple_job_CC_script.sh
@@ -9,7 +9,7 @@
 
 # ppo script
 for lr_a in {1e-4}; do
-    for lr_c in {1e-4} do
+    for lr_c in {1e-4}; do
         for entropy_coeff in {5e-2,5e-3,5e-4}; do
             sbatch CC_script-ppo.sh $entropy_coeff $lr_a $lr_c
         done
