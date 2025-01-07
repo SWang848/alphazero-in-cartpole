756eb1a2d80bb00b448822285f8e30b587a129c3
SWang848 <swang848@outlook.com>
2025-01-06 23:53:34 -0700

    # c15b swap MDP; ppo

--- multiple_job_CC_script.sh
+++ multiple_job_CC_script.sh
@@ -1,18 +1,18 @@
 #!bin/bash
 
 # mcts script
-for c_init in {2,3,4}; do
-    for num_simulations in {120,150}; do
-        sbatch CC_script.sh $c_init $num_simulations
-    done
-done
-
-# ppo script
-# for lr_a in {1e-4}; do
-#     for lr_c in {1e-4} do
-#         for entropy_coeff in {5e-2,5e-3,5e-4}; do
-#             sbatch CC_script-ppo.sh $entropy_coeff $lr_a $lr_c
-#         done
+# for c_init in {2,3,4}; do
+#     for num_simulations in {120,150}; do
+#         sbatch CC_script.sh $c_init $num_simulations
 #     done
 # done
+
+# ppo script
+for lr_a in {1e-4}; do
+    for lr_c in {1e-4} do
+        for entropy_coeff in {5e-2,5e-3,5e-4}; do
+            sbatch CC_script-ppo.sh $entropy_coeff $lr_a $lr_c
+        done
+    done
+done
 
