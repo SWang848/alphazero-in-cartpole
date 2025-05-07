0ef334b75d27b2ba7decf8a781db5ecdbadf6065
SWang848 <swang848@outlook.com>
2025-05-07 09:24:03 -0600

    scrip update

--- CC_script.sh
+++ CC_script.sh
@@ -16,6 +16,7 @@ echo $1 # seed
 export CUBLAS_WORKSPACE_CONFIG=:4096:8
 export WANDB_MODE=offline # log offline
 export WANDB__SERVICE_WAIT=300
+export RAY_memory_usage_threshold=0.95
 export VTR_ROOT=/home/shang8/scratch/vtr-verilog-to-routing
 export results=$SLURM_TMPDIR/results
 cp -R /home/shang8/scratch/alphazero-in-cartpole/data $SLURM_TMPDIR/data
@@ -31,6 +32,6 @@ wandb offline
 
 ray start --head --node-ip-address=$HEAD_NODE --port=$RAY_PORT --include-dashboard=false --metrics-export-port=0 --num-cpus=12 --num-gpus=1
 
-PYTHONUNBUFFERED=1 python3 -u main.py --cc --wandb --group_name c15b_gumbel_cc_NFI --amp --env Swap-v0 --seed $1 --non_fixed_init --num_rollout_workers 10 --num_cpus_per_worker 1.2 --num_envs_per_worker 20 --num_gpus_per_worker 0.1 --min_num_episodes_per_worker 20 --num_target_blocks 15 --num_simulations 75 --training_steps 60 --c_init 2.5 --lr 1e-3 --value_support_min -10 --value_support_max 0 --value_support_delta 1 --m_top 8 --c_visit 36 --c_init 0.1
+PYTHONUNBUFFERED=1 python3 -u main.py --cc --wandb --group_name c15b_gumbel_cc --amp --env Swap-v0 --seed 12 --num_rollout_workers 10 --num_cpus_per_worker 1.2 --num_envs_per_worker 20 --num_gpus_per_worker 0.1 --min_num_episodes_per_worker 20 --num_target_blocks 15 --num_simulations 75 --training_steps 60 --c_init 2.5 --lr 1e-3 --value_support_min -10 --value_support_max 0 --value_support_delta 1 --m_top 8 --c_visit 36 --c_init 0.1
 
 cp -r $results/* /home/shang8/scratch/alphazero-in-cartpole/results/
