b78416a04fbac20c7abf1f2a03c81abee05801ee
SWang848 <swang848@outlook.com>
2025-05-06 18:37:45 -0600

    update ccscript

--- CC_script.sh
+++ CC_script.sh
@@ -29,8 +29,7 @@ module load cuda
 source /home/shang8/scratch/MCTS_env/bin/activate
 wandb offline
 
-ray start --head --node-ip-address=$HEAD_NODE --port=$RAY_PORT --num-cpus=12 --num-gpus=1 --block &
-sleep 20
+ray start --head --node-ip-address=$HEAD_NODE --port=$RAY_PORT --include-dashboard=false --metrics-export-port=0 --num-cpus=12 --num-gpus=1
 
 PYTHONUNBUFFERED=1 python3 -u main.py --cc --wandb --group_name c15b_gumbel_cc --amp --env Swap-v0 --seed $1 --num_rollout_workers 10 --num_cpus_per_worker 1.2 --num_envs_per_worker 20 --num_gpus_per_worker 0.1 --min_num_episodes_per_worker 20 --num_target_blocks 15 --num_simulations 75 --training_steps 60 --c_init 2.5 --lr 1e-3 --value_support_min -10 --value_support_max 0 --value_support_delta 1 --m_top 8 --c_visit 36 --c_init 0.1
 
