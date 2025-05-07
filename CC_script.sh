88ba2c2ee3bf68a3ad576ed9568cb02de6981240
SWang848 <swang848@outlook.com>
2025-05-07 12:34:34 -0600

    update script

--- CC_script.sh
+++ CC_script.sh
@@ -32,6 +32,6 @@ wandb offline
 
 ray start --head --node-ip-address=$HEAD_NODE --port=$RAY_PORT --include-dashboard=false --metrics-export-port=0 --num-cpus=12 --num-gpus=1
 
-PYTHONUNBUFFERED=1 python3 -u main.py --cc --wandb --group_name c30b_gumbel_cc_NFI  --amp  --env Swap-v0 --seed $1 --non_fixed_init --num_rollout_workers 10 --num_cpus_per_worker 1.2 --num_envs_per_worker 20 --num_gpus_per_worker 0.1 --min_num_episodes_per_worker 20 --num_target_blocks 30 --num_simulations 150 --training_steps 60 --c_init 2.5 --lr 1e-3 --value_support_min -10 --value_support_max 0 --value_support_delta 1 --m_top 16 --c_visit 41 --c_init 0.1
+PYTHONUNBUFFERED=1 python3 -u main.py --cc --wandb --group_name c30b_gumbel_cc_NFI  --amp  --env Swap-v0 --seed 723 --non_fixed_init --num_rollout_workers 10 --num_cpus_per_worker 1.2 --num_envs_per_worker 20 --num_gpus_per_worker 0.1 --min_num_episodes_per_worker 20 --num_target_blocks 30 --num_simulations 150 --training_steps 60 --c_init 2.5 --lr 1e-3 --value_support_min -10 --value_support_max 0 --value_support_delta 1 --m_top 16 --c_visit 41 --c_init 0.1
 
 cp -r $results/* /home/shang8/scratch/alphazero-in-cartpole/results/
