2ca1fd9ec95b990b12231639e3c7b0fdbedb79fc
SWang848 <swang848@outlook.com>
2025-05-20 16:00:37 -0600

    c5b_gumbel_RL4RS

--- CC_script.sh
+++ CC_script.sh
@@ -41,5 +41,5 @@ wandb offline
 export sub_dir="seed_$((RANDOM % 10000))"
 ray start --head --node-ip-address=$HEAD_NODE --port=$RAY_PORT --include-dashboard=false --metrics-export-port=0 --num-cpus=12 --num-gpus=1
 
-PYTHONUNBUFFERED=1 python3 -u main.py --cc --wandb --group_name c5b_gumbel_RL4RS --amp --env Classic-v0 --seed 0 --num_rollout_workers 10 --num_cpus_per_worker 1.2 --num_envs_per_worker 20 --num_gpus_per_worker 0.1 --min_num_episodes_per_worker 20 --num_target_blocks 5 --place_order default --num_simulations 10 --max_training_time 3600  --c_init 2 --lr 5e-4 --value_support_min -1 --value_support_max 0 --value_support_delta 0.1 --m_top 4 --c_visit 8 --c_scale 1
+PYTHONUNBUFFERED=1 python3 -u main.py --cc --wandb --group_name c5b_gumbel_RL4RS --amp --env Classic-v0 --seed 0 --num_rollout_workers 10 --num_cpus_per_worker 1.2 --num_envs_per_worker 20 --num_gpus_per_worker 0.1 --min_num_episodes_per_worker 20 --num_target_blocks 5 --place_order connections_ --num_simulations 10 --max_training_time 3600  --c_init 2 --lr 5e-4 --value_support_min -1 --value_support_max 0 --value_support_delta 0.1 --m_top 4 --c_visit 8 --c_scale 1
 cp -r $results/* /home/shang8/scratch/alphazero-in-cartpole/results/
