733c57471c96ff0fd07bf5a900180677a399ee8e
SWang848 <swang848@outlook.com>
2025-05-20 21:48:40 -0600

    c15b_gumbel_RL4RS

--- CC_script.sh
+++ CC_script.sh
@@ -2,7 +2,7 @@
 #SBATCH --nodes=1
 #SBATCH --gpus-per-node=1
 #SBATCH --ntasks-per-node=12
-#SBATCH --mem=20G
+#SBATCH --mem=60G
 #SBATCH --time=72:00:00
 #SBATCH --account=rrg-mtaylor3
 #SBATCH --output=/home/shang8/scratch/slurm_out/%A.out
@@ -42,5 +42,5 @@ wandb offline
 export sub_dir="seed_$((RANDOM % 10000))"
 ray start --head --node-ip-address=$HEAD_NODE --port=$RAY_PORT --include-dashboard=false --metrics-export-port=0 --num-cpus=12 --num-gpus=1
 
-PYTHONUNBUFFERED=1 python3 -u main.py --cc --wandb --group_name c5b_gumbel_RL4RS --amp --env Classic-v0 --seed 0 --num_rollout_workers 10 --num_cpus_per_worker 1.2 --num_envs_per_worker 20 --num_gpus_per_worker 0.1 --min_num_episodes_per_worker 20 --num_target_blocks 5 --place_order $1 --num_simulations 50 --max_training_time 3600 --c_init 2 --lr 5e-4 --value_support_min -1 --value_support_max 0 --value_support_delta 0.1 --m_top 4 --c_visit 8 --c_scale 1
+PYTHONUNBUFFERED=1 python3 -u main.py --cc --wandb --group_name c15b_gumbel_RL4RS --amp --env Classic-v0 --seed 0 --num_rollout_workers 10 --num_cpus_per_worker 1.2 --num_envs_per_worker 20 --num_gpus_per_worker 0.1 --min_num_episodes_per_worker 20 --num_target_blocks 15 --place_order $1 --num_simulations 100 --max_training_time 28800 --c_init 2 --lr 5e-4 --value_support_min -1 --value_support_max 0 --value_support_delta 0.1 --m_top 8 --c_visit 36 --c_scale 1
 cp -r $results/* /home/shang8/scratch/alphazero-in-cartpole/results/
